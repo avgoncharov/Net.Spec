489eb4c532bc418faf8ebd3ceaf327f9e320775b
avgoncharov <goncharov.andrey.v@gmail.com>
2018-01-18 23:26:30 +0300

    Добавил тесты.

---
+++ Tests/Net.Spec.Logging.Tests/TestSink.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Text;
+
+namespace Net.Spec.Logging.Tests
+{
+	public class TestSink : ILogEventSink
+	{
+		private StringBuilder _strBldr;
+		public TestSink(StringBuilder strBldr)
+		{
+			_strBldr = strBldr;
+		}
+
+		public void Emit(LogEvent logEvent)
+		{
+			_strBldr.AppendLine(logEvent.RenderMessage());
+		}
+	}
+
+	public static class TestSinkExtensions
+	{
+		public static LoggerConfiguration TestSink(
+			this LoggerSinkConfiguration loggerConfiguration,
+			StringBuilder strBld,
+			IFormatProvider formatProvider = null)
+		{
+			return loggerConfiguration.Sink(new TestSink(strBld));
+		}
+	}
+}
